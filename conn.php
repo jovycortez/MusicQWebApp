6e215acc5f7957b7cfcb3b6a39f1754a90af475f
Jovy Cortez <jovycortez11@yahoo.com>
2016-04-14 21:08:30 -0400

    Connection test

--- conn.php
+++ conn.php
@@ -1,1 +1,16 @@
-<?php dwhffdhuifdhh?>
+<?php
+$servername = "us-cdbr-azure-east-c.cloudapp.net";
+$username = "b1223efdce4428";
+$password = "86dec4d4";
+$dbname = "MusicQdb";
+
+$conn = new mysqli($servername, $username, $password, $dbname);
+if (!$conn->connect_error) {
+   die("Connection failed: " . $conn->connect_error);
+}else{
+	echo "pass";
+	<h1>pass</h1>
+}
+
+
+?>
